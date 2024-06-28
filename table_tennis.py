1326f2438f346deca8538bf524e4b740bcc08c53
raullucas7 <139395594+raullucas7@users.noreply.github.com>
2024-06-28 17:10:21 -0500

    made main interface and paddles

--- table_tennis.py
+++ table_tennis.py
@@ -1,5 +1,27 @@
 import turtle
-import pygame
 
+def screen_setup ():
+    
+    pg = turtle.Screen()
+    
+    pg.title("Paddle Prodigy")
+    pg.bigcolor("white")
+    pg.setup(width=800, height=600)
+    pg.tracer(0)
+    
+    return pg
 
+def create_paddle(x, y):
+    
+    paddle = turtle.Turtle()
+    
+    paddle.speed(0)
+    paddle.shape("square")
+    paddle.color("black")
+    paddle.shapesize(stretch_wid = 5, stretch_len = 1)
+    
+    paddle.penup()
+    paddle.goto(x, y)
+    
+    return paddle
 
