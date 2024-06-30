5a8f86758317fec28d4137ebad98f0e779c2a1d4
raullucas7 <139395594+raullucas7@users.noreply.github.com>
2024-06-30 14:42:25 -0500

    added file to handle game soundtrack

---
+++ music_handler.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+    
+def pause_music():
+    pygame.mixer.music.pause()
+
+    
+def unpause_music():
+    pygame.mixer.music.unpause()
+
+
+def initialize_soundtrack(soundtrack_path):
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(soundtrack_path)
+    pygame.mixer.music.play(-1)                 # music loops
+
+  
+if __name__ == "__main__":
+    soundtrack_track = "path_to_your_soundtrack.mp3"
+    initialize_soundtrack(soundtrack_path)
+    
+    while True:
+        pass
